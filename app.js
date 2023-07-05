da9a6032cf8e5e47a7ad6311199730197f59c381
hydra <hydradaboss06@gmail.com>
2023-07-05 16:54:49 +0100

    few extra code

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require("dotenv").config()
+require("express-async-errors")
 
 const connectdb = require('./connectdb')
 const app = express()
 
+app.use(express.json())
+
+const port = process.env.PORT
 const start = async () =>{
     try {
         await connectdb()
-        app.listen()
+        app.listen(port)
     } catch (error) {
         console.log(error)
     }
---
+++ controllers/signUpContoller.js
@@ -0,0 +1,3 @@
+const postController = (req,res,next) =>{
+
+}
